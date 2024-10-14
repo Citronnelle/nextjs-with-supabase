cbca198af956f0a9ae1d60168bb93173c3652bac
Taiki-Viia Tungal <taikiviia@gmail.com>
2024-10-14 03:16:54 +0300

    feat: allow everyone to read results for TODOs but only the authenticated to see the form

--- app/todos/page.tsx
+++ app/todos/page.tsx
@@ -2,7 +2,6 @@ import { createClient } from "@/utils/supabase/server";
 import { SubmitButton } from "@/components/submit-button";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { redirect } from "next/navigation";
 
 export default async function Index() {
   const supabase = createClient();
@@ -10,10 +9,6 @@ export default async function Index() {
   const {
     data: { user },
   } = await supabase.auth.getUser();
-
-  if (!user) {
-    return redirect("/sign-in");
-  }
 
   type Todo = {
     id: string;
@@ -66,24 +61,26 @@ export default async function Index() {
   };
 
   let todos = await fetchTodos();
-  if (!todos || todos.length === 0) {
-    return <h1>No todos found</h1>;
-  }
 
   return (
     <>
       <main className="flex-1 flex flex-col gap-6 px-4">
-        <pre>{JSON.stringify(todos, null, 2)}</pre>
-
-        <form className="flex-1 flex flex-col min-w-64">
-          <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
-            <Label htmlFor="title">Title</Label>
-            <Input name="title" required />
-            <Label htmlFor="priority">Priority</Label>
-            <Input type="number" name="priority" required />
-            <SubmitButton pendingText="Lisamine...">LISA</SubmitButton>
-          </div>
-        </form>
+        {!todos || todos.length === 0 ? (
+          <h3>No todos found</h3>
+        ) : (
+          <pre>{JSON.stringify(todos, null, 2)}</pre>
+        )}
+        {user ? (
+          <form className="flex-1 flex flex-col min-w-64">
+            <div className="flex flex-col gap-2 [&>input]:mb-3 mt-8">
+              <Label htmlFor="title">Title</Label>
+              <Input name="title" required />
+              <Label htmlFor="priority">Priority</Label>
+              <Input type="number" name="priority" required />
+              <SubmitButton pendingText="Lisamine...">LISA</SubmitButton>
+            </div>
+          </form>
+        ) : null}
       </main>
     </>
   );
