57048e0e0ffa6b83ef63d3d115eb8b6ccc74c8c4
Taiki-Viia Tungal <taikiviia@gmail.com>
2024-12-16 07:52:54 +0200

    fix: resolve build errors

--- app/protected/reset-password/page.tsx
+++ app/protected/reset-password/page.tsx
@@ -9,6 +9,8 @@ export default async function ResetPassword({
 }: {
   searchParams: Message;
 }) {
+  const params = await searchParams;
+
   return (
     <form className="flex flex-col w-full max-w-md p-4 gap-2 [&>input]:mb-4">
       <h1 className="text-2xl font-medium">Reset password</h1>
@@ -32,7 +34,7 @@ export default async function ResetPassword({
       <SubmitButton formAction={resetPasswordAction}>
         Reset password
       </SubmitButton>
-      <FormMessage message={searchParams} />
+      <FormMessage message={params} />
     </form>
   );
 }
