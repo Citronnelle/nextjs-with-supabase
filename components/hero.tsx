cd946f52640b88627895b48b9e6c584b15cf9c5f
Taiki-Viia Tungal <taikiviia@gmail.com>
2024-12-16 10:05:15 +0200

    chore: clean up excess elements & make TODOs more accessible

--- components/hero.tsx
+++ components/hero.tsx
@@ -1,44 +1,35 @@
-import NextLogo from "./next-logo";
-import SupabaseLogo from "./supabase-logo";
+"use client";
+
+import { useUser } from "@/app/context-provider";
+import Link from "next/link";
 
 export default function Header() {
+  const { user } = useUser();
+
   return (
-    <div className="flex flex-col gap-16 items-center">
-      <div className="flex gap-8 justify-center items-center">
-        <a
-          href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <SupabaseLogo />
-        </a>
-        <span className="border-l rotate-45 h-6" />
-        <a href="https://nextjs.org/" target="_blank" rel="noreferrer">
-          <NextLogo />
-        </a>
+    <>
+      <div className="flex flex-col gap-16 items-center">
+        <div className="flex gap-8 justify-center items-center"></div>
+        <div className="text-3xl lg:text-4xl !leading-tight mx-auto max-w-xl text-center">
+          {!user && (
+            <h1>
+              <Link href={"/sign-in"} className="font-extrabold">
+                Log in
+              </Link>{" "}
+              to add TODOs!
+            </h1>
+          )}
+          {user && (
+            <h1>
+              <Link href={"/todos"} className="font-extrabold">
+                Go here
+              </Link>{" "}
+              to add TODOs!
+            </h1>
+          )}
+        </div>
+        <div className="w-full p-[1px] bg-gradient-to-r from-transparent via-foreground/10 to-transparent my-8" />
       </div>
-      <h1 className="sr-only">Supabase and Next.js Starter Template</h1>
-      <p className="text-3xl lg:text-4xl !leading-tight mx-auto max-w-xl text-center">
-        The fastest way to build apps with{" "}
-        <a
-          href="https://supabase.com/?utm_source=create-next-app&utm_medium=template&utm_term=nextjs"
-          target="_blank"
-          className="font-bold hover:underline"
-          rel="noreferrer"
-        >
-          Supabase
-        </a>{" "}
-        and{" "}
-        <a
-          href="https://nextjs.org/"
-          target="_blank"
-          className="font-bold hover:underline"
-          rel="noreferrer"
-        >
-          Next.js
-        </a>
-      </p>
-      <div className="w-full p-[1px] bg-gradient-to-r from-transparent via-foreground/10 to-transparent my-8" />
-    </div>
+    </>
   );
 }
