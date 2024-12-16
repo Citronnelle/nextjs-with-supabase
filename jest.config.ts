58bf50bf5fe5e921bc3da2884c9e1daf52085891
Taiki-Viia Tungal <taikiviia@gmail.com>
2024-12-16 08:34:37 +0200

    chore: configure testing environment

---
+++ jest.config.ts
@@ -0,0 +1,15 @@
+import type { Config } from "jest";
+import nextJest from "next/jest.js";
+
+const createJestConfig = nextJest({
+  dir: "./",
+});
+
+const config: Config = {
+  clearMocks: true,
+  coverageDirectory: "coverage",
+  coverageProvider: "v8",
+  testEnvironment: "jsdom",
+};
+
+export default createJestConfig(config);
